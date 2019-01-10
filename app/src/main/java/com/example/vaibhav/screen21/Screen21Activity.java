ace85148b5e36ea943094305af2389bbaaae68e9
wsdesign <info.wsdesignuiux@gmail.com>
2019-01-10 17:03:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/screen21/Screen21Activity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.screen21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen21Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen21);
+    }
+}
